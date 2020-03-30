06a73f399aa4e033891310b34b7b7df87bbd8061
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-03-30 14:57:35 +0300

    Доделал дополнительное задание.

---
+++ lesson03/Additional Task/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+//1
+
+let lang = prompt('Вибирите язык.(ru или en)', 'ru').toLowerCase();
+
+const daysOfWeek = [
+    ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+    ['Sumday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+]
+
+//через if...
+
+if(lang === 'ru'){
+    console.log(daysOfWeek[0]);
+}else if(lang === 'en'){
+    console.log(daysOfWeek[1]);
+}else{
+    console.log('Вы ввели не корректное значение.');
+}
+
+//через swich...
+
+switch (lang){
+    case 'ru':
+        console.log(daysOfWeek[0]);
+        break;
+    case 'en':
+        console.log(daysOfWeek[1]);
+        break;
+    default:
+        console.log('Вы ввели не корректное значение.');
+}
+
+//через многомерный массив...
+//не знаю правильно ли понял задание))
+lang === 'ru' ? console.log(daysOfWeek[0]) : console.log(null);
+lang === 'en' ? console.log(daysOfWeek[1]) : console.log(null);
+
+//2
+
+let namePerson = 'маКСИм';
+
+namePerson.toLowerCase() === 'артем' ? console.log('Директор') : console.log('студент');
+namePerson.toLowerCase() === 'максим' ? console.log('Преподаватель') : console.log('студент');
