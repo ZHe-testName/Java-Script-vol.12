4cc9f8bdc42dbc769de6c3ce95e8cee96e379a28
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-03-30 23:35:40 +0300

    Как смог починил дополнительный таск.

--- lesson03/Additional Task/script.js
+++ lesson03/Additional Task/script.js
@@ -34,12 +34,19 @@ switch (lang){
 
 //через многомерный массив...
 //не знаю правильно ли понял задание))
-lang === 'ru' ? console.log(daysOfWeek[0]) : console.log(null);
-lang === 'en' ? console.log(daysOfWeek[1]) : console.log(null);
+let notRu = false;
+let notEn = false;
+
+lang === 'ru' ? console.log(daysOfWeek[0]) : notRu = true;
+lang === 'en' ? console.log(daysOfWeek[1]) : notEn = true;
+(notEn && notRu) ? console.log('Нужно ввести правильное значение!') : (notRu = false, notEn = false);
 
 //2
 
-let namePerson = 'ЫАФФЦУФ';
+let namePerson = 'Артем';
+let temp1 = false;
+let temp2 = false;
 
-namePerson.toLowerCase() === 'артем' ? console.log('Директор') : console.log('студент');
-namePerson.toLowerCase() === 'максим' ? console.log('Преподаватель') : console.log('студент');
+namePerson.toLowerCase() === 'артем' ? console.log('Директор') : temp1 = true;
+namePerson.toLowerCase() === 'максим' ? console.log('Преподаватель') : temp2 = true;
+(temp1 && temp2) ? console.log('Студент') : (temp1 = false,temp2 = false);
