8be0d2b165da313ac8ebe1d0109c9ebf27896e5a
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-03-31 09:19:30 +0300

    Дополнительное задание,попытка номер два.

--- lesson03/Additional Task/script.js
+++ lesson03/Additional Task/script.js
@@ -34,19 +34,14 @@ switch (lang){
 
 //через многомерный массив...
 //не знаю правильно ли понял задание))
-let notRu = false;
-let notEn = false;
 
-lang === 'ru' ? console.log(daysOfWeek[0]) : notRu = true;
-lang === 'en' ? console.log(daysOfWeek[1]) : notEn = true;
-(notEn && notRu) ? console.log('Нужно ввести правильное значение!') : (notRu = false, notEn = false);
+lang === 'ru' ? console.log(daysOfWeek[0]) :
+lang === 'en' ? console.log(daysOfWeek[1]) : console.log('Нужно ввести правильное значение!');
+
 
 //2
 
-let namePerson = 'Артем';
-let temp1 = false;
-let temp2 = false;
+let namePerson = 'макСИМ';
 
-namePerson.toLowerCase() === 'артем' ? console.log('Директор') : temp1 = true;
-namePerson.toLowerCase() === 'максим' ? console.log('Преподаватель') : temp2 = true;
-(temp1 && temp2) ? console.log('Студент') : (temp1 = false,temp2 = false);
+namePerson.toLowerCase() === 'артем' ? console.log('Директор') :
+namePerson.toLowerCase() === 'максим' ? console.log('Преподаватель') : console.log('Студент');
