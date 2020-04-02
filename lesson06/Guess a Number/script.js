e60cf0c01c99cb2c8ff2507facd989616ab69e1b
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-04-02 15:40:21 +0300

    Закончил дополнительное.Никаких циклов.

--- lesson06/Guess a Number/script.js
+++ lesson06/Guess a Number/script.js
@@ -8,23 +8,33 @@ function isItNumber(n){
 
 function letsPlay(){
     let number = Math.floor(Math.random() * 100);
+    let attemptAmount = 10;
     console.log(number);
 
     return function guess(){
         let yourChoise = prompt('Введите предпологаемое число.');
-        
+
         if(isItNumber(yourChoise)){
-            if(yourChoise === number){
+            if(attemptAmount === 1){
+                let failQestion = confirm('Попытки закончились.Может еще разочек?');
+
+                (failQestion === true) ? letsPlay()() : alert('До новых встреч.');
+            }else if(yourChoise == number){
                 alert('Вы угадали!!!');
-                return;
+                let qestion = confirm('Сыграем еще разок?');
+
+                (qestion === true) ? letsPlay()() : alert('До новых встреч.');
             }else if(yourChoise < number){
-                alert('Загаданое число БОЛШЕ Вашего.');
+                attemptAmount--;
+                alert('Загаданое число БОЛШЕ Вашего.Осталось ' + attemptAmount + ' попыток.');
                 guess();
             }else if(yourChoise > number){
-                alert('Загаданое число МЕНЬШЕ Вашего.');    
+                attemptAmount--;
+                alert('Загаданое число МЕНЬШЕ Вашего.Осталось ' + attemptAmount + ' попыток.');    
                 guess();   
             }
         }else if(yourChoise == null){
+            alert('До новых встреч.');
             return;
         }else{
             alert('Водите толко ЧИСЛА!!!');
@@ -33,7 +43,7 @@ function letsPlay(){
     }
 }
 
-let Game = letsPlay();
+const Game = letsPlay();
 
 Game();
 
