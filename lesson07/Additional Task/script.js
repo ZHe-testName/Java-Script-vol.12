b0307de359e27715cdf8f91ffafd4724c2b18726
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-04-03 12:59:08 +0300

    Полностю закончил 7-ую домашку.

--- lesson07/Additional Task/script.js
+++ lesson07/Additional Task/script.js
@@ -1,3 +1,27 @@
 'use strict'
 
-console.log('hi!');
+const week = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thurday',
+    'Friday',
+    'Saturday'
+];
+
+let now = new Date().getDay();
+
+document.write(week + '<br/><br/>');
+
+week.forEach(function(item, i){
+    if(item === 'Saturday' || item === 'Sunday'){
+        document.write(`<em>${item}</em>` + '<br/>');
+    }else if(now === i){
+        document.write(`<strong>${item}</strong>` + '<br/>');
+    }else{
+        document.write(item + '<br/>');
+    }
+});
+
+
