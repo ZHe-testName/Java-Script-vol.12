7ede8269c0279df863a189514dca31509ba19feb
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-04-16 17:59:45 +0300

    All methods replased to protoype.

--- lesson14/Main Task ex.1/script.js
+++ lesson14/Main Task ex.1/script.js
@@ -7,58 +7,8 @@ const DomElement = function(selector, height, width, bgCol, fonSize){
     this.width = width + 'px';
     this.bgCol = bgCol;
     this.fonSize = fonSize + 'px';
-
-
-    this.createElement = function(){
-        let element;
-        if(this.selector[0] === '.'){
-            element = document.createElement('div');
-            element.classList.add(this.selector.slice(1));
-            element.innerText = "Привет! Я <div>.";
-        }else if(this.selector[0] === '#'){
-            element = document.createElement('p');
-            element.classList.add(this.selector.slice(1));
-            element.innerText = "Привет! Я <p>.";
-        }
-
-        element.style.cssText = `
-                                position: ${this.position};
-                                top: 200px;
-                                left: 700px;
-                                height: ${this.height};
-                                width: ${this.width};
-                                background-color: ${this.bgCol};
-                                font-size: ${this.fonSize};
-                                `
-        document.body.append(element);
-    };
-
-    this.moveElement = function(e){
-        let code = e.keyCode;
-        let elem = document.querySelector(selector);
-        let computedObj = getComputedStyle(elem);
-
-        switch(code){
-            case 40:
-                elem.style.top = (+(computedObj.top.substring(0, computedObj.top.length - 2)) + 10) + 'px';
-                break;
-            case 38:
-                elem.style.top = (+(computedObj.top.substring(0, computedObj.top.length - 2)) - 10) + 'px';
-                break;
-            case 37:
-                elem.style.left = (+(computedObj.left.substring(0, computedObj.left.length - 2)) - 10) + 'px';
-                break;
-            case 39:
-                elem.style.left = (+(computedObj.left.substring(0, computedObj.left.length - 2)) + 10) + 'px';
-                break;
-        }
-    }
 };
 
-
-//Или лучше в прототип писать???
-
-/*
 DomElement.prototype.createElement= function(){
         let element;
         if(this.selector[0] === '.'){
@@ -72,8 +22,11 @@ DomElement.prototype.createElement= function(){
         }
 
         element.style.cssText = `
+                                position: ${this.position};
                                 height: ${this.height};
                                 width: ${this.width};
+                                top: 200px;
+                                left: 700px;
                                 background-color: ${this.bgCol};
                                 font-size: ${this.fonSize};
                                 `
@@ -81,10 +34,8 @@ DomElement.prototype.createElement= function(){
     };
 
 DomElement.prototype.moveElement = function(e){
-        const _this = DomElement.selector;
         let code = e.keyCode;
-        let elem = document.querySelector(_this);
-        console.log(elem);
+        let elem = document.querySelector(this.selector);
         let computedObj = getComputedStyle(elem);
 
         switch(code){
@@ -102,12 +53,12 @@ DomElement.prototype.moveElement = function(e){
                 break;
         }
     };
-*/
+
 
 const newDiv = new DomElement('.block', 100, 100, 'red', 20);
 
 document.addEventListener("DOMComputedLoaded", newDiv.createElement());
-document.addEventListener("keydown", newDiv.moveElement);
+document.addEventListener("keydown", newDiv.moveElement.bind(newDiv));
 
 //const newPar = new DomElement('#best', 60, 700, 'green', 25);
 //newPar.createElement();
