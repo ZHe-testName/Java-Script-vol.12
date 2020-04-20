ecded0eca610d651080e29a090f0c235606956a5
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-04-20 09:10:06 +0300

    All files are download, practic part is comlete.

---
+++ lesson18/scripts/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function(){
+
+    //Timer function
+    function countTimer(deadline){
+        let timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaning(){
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaning = (dateStop - dateNow) / 1000,
+
+                seconds = Math.floor(timeRemaning % 60),
+                minutes = Math.floor((timeRemaning / 60) % 60),
+                hours = Math.floor(timeRemaning / 60 / 60);
+
+                return {hours, minutes, seconds, timeRemaning};
+        }
+    
+        function updateClock(){
+            let timer = getTimeRemaning();
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+            if(timer.timeRemaning > 0){
+                setTimeout(updateClock, 1000);
+            }
+        }
+
+        updateClock();
+    }
+
+    countTimer('22 April 2020');
+    
+});
+
