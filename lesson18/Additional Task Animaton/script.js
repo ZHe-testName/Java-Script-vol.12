337998d568aef62d50c3fc62f0dfab0b3e03a864
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-04-20 21:11:37 +0300

    Finished Additional Task.Inserted animation.

--- lesson18/Additional Task Animaton/script.js
+++ lesson18/Additional Task Animaton/script.js
@@ -1,3 +1,68 @@
 'use strict';
 
-console.log('hi!');
+const smile = document.querySelector('#smile'),
+    explosion = document.querySelector('#explosion'),
+    reset = document.querySelector('#reset'),
+    playPause = document.querySelector('#playPause');
+
+let growingIntrval;
+let count = 100;
+
+function growing(){
+
+        growingIntrval = requestAnimationFrame(growing);
+        count += 2;
+    
+        setTimeout(function(){
+            smile.classList.add('animated', 'shake');
+        }, 900);
+    
+        if(count < 400){
+            smile.style.width = count + 'px';
+            smile.style.height = count + 'px';
+        }else{
+            smile.classList.remove('animated', 'shake');
+    
+            setTimeout(function(){
+                smile.classList.add('animated', 'shake');
+            }, 500);
+            setTimeout(function(){
+                smile.classList.add('invisible');
+                explosion.classList.add('bang');
+                playPause.setAttribute('disabled', 'disabled');
+            }, 1000);
+            count = 100;
+            cancelAnimationFrame(growingIntrval);
+        }
+   
+}
+
+function rePlay(){
+    playPause.classList.toggle('play');
+    playPause.classList.toggle('pause');
+    explosion.classList.remove('bang');
+    smile.classList.remove('animated', 'shake', 'invisible');
+    smile.style.width = count + 'px';
+    smile.style.height = count + 'px';
+    playPause.removeAttribute('disabled');
+}
+
+function pause(){
+    cancelAnimationFrame(growingIntrval);
+
+    playPause.classList.toggle('play');
+    playPause.classList.toggle('pause');
+}
+
+console.log(playPause.className === 'button play');
+
+reset.addEventListener('click', rePlay);
+playPause.addEventListener('click', function(){
+    if(playPause.className === 'button play'){
+        growing();
+        playPause.classList.toggle('play');
+        playPause.classList.toggle('pause');
+    }else if(playPause.className === 'button pause'){
+        pause();
+    }
+});
