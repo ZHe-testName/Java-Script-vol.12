d8801954d9b2b1f354219d7fe3e5f558598b4267
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-03 13:29:41 +0300

    Incerted script with Promise API.

--- Actors Cards/dbHeroes-master/script.js
+++ Actors Cards/dbHeroes-master/script.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('hi my heroes!');
+window.addEventListener('DOMContentLoaded', () => {
+    const getData = () => {
+        return new Promise ((resolve, reject) => {
+            const request = new XMLHttpRequest();
+
+            request.open('GET', './dbHeroes.json');
+
+            request.addEventListener('readystatechange', () => {
+                if(request.readyState !== 4){
+                    return;
+                }
+
+                if(request.status === 200){
+                    const response = JSON.parse(request.responseText);
+                    resolve(response);
+                }else{
+                    reject('Error happend!');
+                }
+            });
+
+            request.send();
+        })
+    };
+
+    const heroObj = getData();
+
+    let set = new Set;
+    
+    heroObj
+        .then(arr => {
+            arr.forEach(item => {
+                    if(item.movies){
+                        item.movies.forEach(elem => {
+                            set.add(elem);
+                        })
+                    }
+                })
+            })
+        .then(console.log(set))
+        .catch(error => console.error(error));
+
+});
