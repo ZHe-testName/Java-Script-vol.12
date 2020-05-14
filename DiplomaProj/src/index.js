dd0619198f6e5fcc71681ef699d105f6d860effe
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-14 17:59:52 +0300

    Includes and adjust wrbpack & babel.

---
+++ DiplomaProj/src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+//All header manioulations functional
+import headerActons from './modules/header';
+
+
+headerActons();
---
+++ DiplomaProj/src/modules/header.js
@@ -0,0 +1,7 @@
+const headerActons = () => {
+    console.log('hi Im here, bitch!');
+
+    localStorage.removeItem('countryData');
+};
+
+export default headerActons;
