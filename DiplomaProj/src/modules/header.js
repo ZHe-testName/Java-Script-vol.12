e1f88cd798a4c21ce0fd24b9b2a634e24118460c
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-14 20:09:09 +0300

    Incert rotation phone arrow.

--- DiplomaProj/src/modules/header.js
+++ DiplomaProj/src/modules/header.js
@@ -1,7 +1,30 @@
 const headerActons = () => {
-    console.log('hi Im here, bitch!');
+    const header = document.querySelector('.header'),
+        phoneNumber = document.querySelector('.header-contacts__phone-number-accord'),
+        headerArrow = document.querySelector('.header-contacts__arrow');
 
-    localStorage.removeItem('countryData');
+    let dirFlag = false;
+
+    header.addEventListener('click', (event) => {
+        let target = event.target;
+        
+        if(target.classList.contains('header-contacts__arrow') || target.id === 'headerArrow'){
+            if(!dirFlag){
+                phoneNumber.style.marginTop = '25px';
+                phoneNumber.firstChild.style.opacity = '1';
+    
+                headerArrow.classList.add('rotate-header-arrow');
+                dirFlag = !dirFlag;
+            }else{
+                phoneNumber.style.marginTop = '';
+                phoneNumber.firstChild.style.opacity = '0';
+
+                headerArrow.classList.remove('rotate-header-arrow');
+                dirFlag = !dirFlag;
+            }
+           
+        }
+    })
 };
 
 export default headerActons;
