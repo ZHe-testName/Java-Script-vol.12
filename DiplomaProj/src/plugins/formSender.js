54824ac1592688ca66aea89709f4aa1907105d11
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-16 01:49:20 +0300

    Include phone mask for inputs.

--- DiplomaProj/src/plugins/formSender.js
+++ DiplomaProj/src/plugins/formSender.js
@@ -56,6 +56,8 @@ class Sender{
                 this.wordsValidator(target);
             }else if(target.getAttribute('name') === 'phone'){
                 this.numsValidator(target);
+                this.maskPhone('input');
+
             }
         })
 
@@ -78,7 +80,46 @@ class Sender{
         this.wordsValidator = (target) => {
             this.val = target.value;
             target.value = this.val.replace(/[^а-я\s\.]/, '');
-        }
+        };
+
+        this.maskPhone = (selector, masked = '+7 (___) ___-__-__') => {
+            const elems = document.querySelectorAll(selector);
+        
+            function mask(event) {
+                const keyCode = event.keyCode;
+                const template = masked,
+                    def = template.replace(/\D/g, ""),
+                    val = this.value.replace(/\D/g, "");
+                console.log(template);
+                let i = 0,
+                    newValue = template.replace(/[_\d]/g, function (a) {
+                        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+                    });
+                i = newValue.indexOf("_");
+                if (i != -1) {
+                    newValue = newValue.slice(0, i);
+                }
+                let reg = template.substr(0, this.value.length).replace(/_+/g,
+                    function (a) {
+                        return "\\d{1," + a.length + "}";
+                    }).replace(/[+()]/g, "\\$&");
+                reg = new RegExp("^" + reg + "$");
+                if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+                    this.value = newValue;
+                }
+                if (event.type == "blur" && this.value.length < 5) {
+                    this.value = "";
+                }
+        
+            }
+        
+            for (const elem of elems) {
+                elem.addEventListener("input", mask);
+                elem.addEventListener("focus", mask);
+                elem.addEventListener("blur", mask);
+            }
+            
+        };
 
     };
 } 
